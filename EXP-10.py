3fb779beedef92d26abee8ef7dd5dfc7c0dd2dc5
ujWal5678 <thadakaraujwal2006@gmail.com>
2026-02-16 08:51:22 +0530

    Add line and bar plots for monthly sales data

---
+++ EXP-10.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sales_data = pd.read_csv('sales1.csv')
+
+monthly_sales = sales_data.groupby('Month_sales')['Total'].sum()
+
+plt.figure()
+plt.plot(monthly_sales.index, monthly_sales.values)
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.title("Monthly Sales Line Plot")
+plt.show()
+
+
+#2
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sales_data = pd.read_csv('sales1.csv')
+
+monthly_sales = sales_data.groupby('Month_sales')['Total'].sum()
+
+plt.figure()
+plt.bar(monthly_sales.index, monthly_sales.values)
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.title("Monthly Sales Bar Plot")
+plt.show()
